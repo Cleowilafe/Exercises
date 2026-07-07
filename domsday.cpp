6c2eac2d1b3db1fca0abff277759893f158751a9
Clenilson Willames Alves Feitosa <willamesclenilson@gmail.com>
2026-07-07 13:27:43 -0300

    Update domsday.cpp

--- domsday.cpp
+++ domsday.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 void X_n (int b, int t, int m, int v, int n){
     int xn;   // resp
     
     xn = (( n * v) + b) % m;      
-    cout << xn << endl;
-    
+
     if (t != 0) {
         X_n (b, t-1, m, v, xn);
+    } else {
+         cout << xn << endl;
     }
 }
-
 int main() {
+       
     int b = 3;  // incremento
     int t = 10; // times
     int m = 100; // mod
@@ -21,5 +23,5 @@ int main() {
 
  X_n( b, t, m, v, n);
     return 0;
+    
 }
-
